3f63a3b1b330c03d55dd26d324128005095a86a5
Jude Sajith <65233121+SajithJude@users.noreply.github.com>
2023-03-06 20:33:45 +0000

    ss

--- app.py
+++ app.py
@@ -129,7 +129,7 @@ elif choose == "Promotion Ideas":
 # Define the options for the select box
         options = [' That Would arrouse and turn on the guy who is reading it and make him ask for nudes ', ' that Would grab the attention of the person who is reads it and well suits the caption ', ' a sarcastic response']
         des=st.text_area(label='Description',label_visibility='collapsed' ,placeholder='Specify under which context')
-        promp = "Generate a " + selected_option + ' ' + string_list[options.index(selected_option)] + ' Under the following context' + user_input
+        promp = "Generate a " + selected_option + ' ' + options[string_list.index(selected_option)] + ' Under the following context' + user_input
         if st.button('Submit'):
             reply = openai.Completion.create(
                                         engine="text-davinci-003",
